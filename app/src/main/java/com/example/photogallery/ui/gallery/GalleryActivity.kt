1dfac01f37a42a52c891f67a168e39e6495e7643
Nikolui333 <semenovnikolu@yandex.ru>
2024-09-01 12:10:42 +0300

    LazyColumn as a table

--- app/src/main/java/com/example/photogallery/ui/gallery/GalleryActivity.kt
+++ app/src/main/java/com/example/photogallery/ui/gallery/GalleryActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -54,9 +61,25 @@ fun GreetingPreview2() {
 fun SimpleLazyColumn() {
     val items = List(100) { "Item #$it" } // Генерация списка из 100 элементов
 
-    LazyColumn {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3), // Устанавливаем 3 столбца
+        contentPadding = PaddingValues(8.dp) // Отступы вокруг сетки
+    ) {
+        items(items) { item ->
+            Text(
+                text = item,
+                modifier = Modifier
+                    .padding(8.dp) // Отступы для каждой ячейки
+                    .background(MaterialTheme.colorScheme.primary) // Цвет фона ячейки (по желанию)
+                    .fillMaxWidth() // Заполнение ширины ячейки
+            )
+        }
+    }
+    
+
+/*    LazyColumn {
         items(items) { item ->
             Text(text = item, modifier = Modifier.padding(16.dp))
         }
-    }
+    }*/
 }
