814e97f063c22ef1ac71b20df7b6a26664a655cc
Nikolui333 <semenovnikolu@yandex.ru>
2024-09-01 12:05:15 +0300

    adding LazyColumn

--- app/src/main/java/com/example/photogallery/ui/gallery/GalleryActivity.kt
+++ app/src/main/java/com/example/photogallery/ui/gallery/GalleryActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.photogallery.ui.details.ui.theme.PhotoGalleryTheme
 
 class GalleryActivity : ComponentActivity() {
@@ -19,13 +22,14 @@ class GalleryActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PhotoGalleryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+/*                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting2(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    )*/
+                    SimpleLazyColumn()
                 }
-            }
+            //}
         }
     }
 }
@@ -43,5 +47,16 @@ fun Greeting2(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview2() {
     PhotoGalleryTheme {
         Greeting2("Android")
+    }
+}
+
+@Composable
+fun SimpleLazyColumn() {
+    val items = List(100) { "Item #$it" } // Генерация списка из 100 элементов
+
+    LazyColumn {
+        items(items) { item ->
+            Text(text = item, modifier = Modifier.padding(16.dp))
+        }
     }
 }
